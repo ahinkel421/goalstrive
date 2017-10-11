418ec27296e49e059a2dcb2f85f157d881dee3f7
Adam Hinkel <ahinkel421@gmail.com>
2017-10-11 13:36:34 -0400

    responsive updates

--- test/test-goals.js
+++ test/test-goals.js
@@ -90,25 +90,28 @@ describe('Goal API resource', function() {
 		});
 	});
 
-	// describe('DELETE endpoint', function() {
-	// 	it('should delete a goal by id', function() {
-	// 		let goal;
+	describe('DELETE endpoint', function() {
+		it('should delete a goal by id', function() {
+			let goal;
 
-	// 		return Goal
-	// 		.findOne()
-	// 		.then(function(_goal) {
-	// 			goal = _goal;
-	// 			return chai.request(app).delete(`/${goal.id}`);
-	// 		})
-	// 		.then(function(res) {
-	// 			res.should.have.status(204);
-	// 			return Goal.findById(goal.id);
-	// 		})
-	// 		.then(function(_goal) {
-	// 			should.not.exist(_goal);
-	// 		});
-	// 	});
-	// });
+			return Goal
+			.findOne()
+			.then(function(_goal) {
+				goal = _goal;
+				console.log(goal._id)
+				return chai.request(app)
+				.delete(`/api/goals/${goal._id}`)
+				.set('Authorization', `Bearer ${test_token}`)
+			})
+			.then(function(res) {
+				res.should.have.status(204);
+				return Goal.findById(goal._id);
+			})
+			.then(function(_goal) {
+				should.not.exist(_goal);
+			});
+		});
+	});
 
 	describe('POST endpoint', function() {
 
